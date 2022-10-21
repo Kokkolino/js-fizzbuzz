308fc513a653026f64c21e880cbdbf8cd66ea45d
Francesco C <fran461j@outlook.it>
2022-10-21 21:18:37 +0200

    u cant glitch my code boy

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,32 @@ let zz;
 let check3;
 let check5;
 const container = document.getElementById('container');
+const btnStart = document.getElementById('start');
+const btnClear = document.getElementById('clear');
+btnStart.addEventListener('click', start);
+btnStart.addEventListener('click', disableButton);
+btnClear.addEventListener('click', clearBox);
+btnClear.addEventListener('click', enableButton);
+
+
 function start(){
     z = parseInt(document.getElementById('inputFirstNumber').value);
     zz = parseInt(document.getElementById('inputSecondNumber').value);
+if (z > 0 || zz > 0){
+    
+    
+    
+    if (z>zz){
+        x=z;
+        z=zz;
+        zz=x;
+        alert("lo sapevo che avresti provato a trovare il pelo nell'uovo :D");
+    }
+    
     if (z <= 0){
         z=1;
     }
-    if (z>zz){
-    x=z;
-    z=zz;
-    zz=x;
-    alert("lo sapevo che avresti provato a trovare il pelo nell'uovo :D");
-    }
+    
     for (let i = z; i <= zz; ++i) {
         let div = document.createElement('div');
         div.setAttribute('id', i );
@@ -36,19 +50,38 @@ function start(){
             div.setAttribute('class','multiple5 cell')
             document.getElementById('container').appendChild(div);
             document.getElementById(i).innerHTML = `<span>Buzz</span>`;
-
+            
         }
         else{
             div.setAttribute('class','noMultiple cell')
             document.getElementById('container').appendChild(div);
             document.getElementById(i).innerHTML = `<span>${i}</span>`;
-
+            
         }
         
     }
-
+    
+}
+else{
+    alert("compila i campi richiesti! ")
+}
 }
 
 function clearBox(){
     document.getElementById('container').innerHTML = "";
+    document.getElementById("inputFirstNumber").value = "";
+    document.getElementById("inputSecondNumber").value = "";
+    
 }
+
+function disableButton() {
+    document.getElementById("start").disabled = true;
+    document.getElementById("clear").disabled = false;
+}
+
+function enableButton() {
+    document.getElementById("start").disabled = false;
+    document.getElementById("clear").disabled = true;
+    
+}
+btnClear.disabled = true;
