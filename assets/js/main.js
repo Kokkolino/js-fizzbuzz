fd409c84ac2c0ea0779284084c43d19f4017ef06
Francesco C <fran461j@outlook.it>
2022-10-21 18:27:57 +0200

    fixed for cycle

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,29 +3,15 @@ let y;
 let z;
 let zz;
 const container = document.getElementById('container');
-// creazione elemento
-let div = document.createElement('div');
 function start(){
     z = parseInt(document.getElementById('inputFirstNumber').value);
     zz = parseInt(document.getElementById('inputSecondNumber').value);
     console.log(z,zz)
-    if (z < zz && ((zz - z) % 10) === 0){
-        y=(zz - z) / 10;
-
-        for (i=1; i<=y; i++) {
-            for (j=1; j<=x; j++) {
-                // attributi per l'elemento creato
-                div.setAttribute('id', z );
-                div.setAttribute('class', 'cell');
-                // lo si fa apparire cosi?
-                document.getElementById('container').append(div);
-                z = z + 1;
-                
-            }
-        }
-    }
-    else{
-        alert('il primo numero deve essere maggiore del secondo e la differenza tra i due deve essere multiplo di 10')
-    }
-    console.log(start())
+    for (let i = z; i <= zz; ++i) {
+    let div = document.createElement('div');
+    div.setAttribute('id', i );
+    div.setAttribute('class', 'cell');
+    document.getElementById('container').appendChild(div);
+    
+  }
 }
