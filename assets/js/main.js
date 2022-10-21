a22784a4608ff2692f5c6734e35e48f09c8d433c
Francesco C <fran461j@outlook.it>
2022-10-21 20:10:24 +0200

    improved protection to cheaters :D

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 let x;
-let y;
 let z;
 let zz;
 let check3;
@@ -8,11 +7,15 @@ const container = document.getElementById('container');
 function start(){
     z = parseInt(document.getElementById('inputFirstNumber').value);
     zz = parseInt(document.getElementById('inputSecondNumber').value);
-    console.log(z,zz)
-    if (z === 0){
-        z++;
+    if (z <= 0){
+        z=1;
     }
-    
+    if (z>zz){
+    x=z;
+    z=zz;
+    zz=x;
+    alert("lo sapevo che avresti provato a trovare il pelo nell'uovo :D");
+    }
     for (let i = z; i <= zz; ++i) {
         let div = document.createElement('div');
         div.setAttribute('id', i );
